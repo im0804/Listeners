e8a2dedb346e9ef52d93903d4432c512fc5df045
im0804 <95828796+im0804@users.noreply.github.com>
2022-10-27 22:58:17 +0300

    end of coding

---
+++ app/src/main/java/com/example/listeners/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.listeners;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnLongClickListener {
+    Button btnUp, btnDown;
+    int count, count2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnUp = (Button) findViewById(R.id.btnUp);
+        btnDown = (Button) findViewById(R.id.btnDown);
+        count = 0;
+        count2 = 0;
+
+        btnUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count++;
+            }
+        });
+
+        btnDown.setOnLongClickListener(this);
+    }
+
+    /*@Override
+    protected void onRestart() {
+        super.onRestart();
+        count = 0;
+        count2 = 0;
+    }*/
+
+    @Override
+    public boolean onLongClick(View view) {
+        count2 += 2;
+        return true;
+    }
+
+    public void result(View view) {
+        Intent si = new Intent(this, resultActivity.class);
+        si.putExtra("upbutton",count);
+        si.putExtra("downbutton",count2);
+        startActivityForResult(si,1);
+    }
+
+    protected void onActivityResult(int resAct, int good, Intent data_back) {
+        super.onActivityResult(resAct, good, data_back);
+        if (data_back != null) {
+            count = data_back.getIntExtra("upbutton", -1);
+            count2 = data_back.getIntExtra("downbutton", -1);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/listeners/resultActivity.java
@@ -0,0 +1,45 @@
+package com.example.listeners;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class resultActivity extends AppCompatActivity {
+    TextView tv;
+    int upbutton, downbutton;
+    Intent gi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        tv = findViewById(R.id.tv);
+
+        gi = getIntent();
+        upbutton = gi.getIntExtra("upbutton", -19);
+        downbutton = gi.getIntExtra("downbutton", -19);
+
+        if (upbutton > downbutton){
+            tv.setText("blue won!!" + "\n"+"blue: "+upbutton + "\n"+"red: "+downbutton );
+        }
+        else if (upbutton < downbutton){
+            tv.setText("red won!!"+ "\n"+"blue: "+upbutton + "\n"+"red: "+downbutton );
+        }
+        else{
+            tv.setText("it's a tie!"+"\n"+"blue: "+upbutton + "\n"+"red: "+downbutton);
+        }
+    }
+
+    public void Back(View view) {
+        upbutton = 0;
+        downbutton = 0;
+        gi.putExtra("upbutton",0);
+        gi.putExtra("downbutton",0);
+        setResult(RESULT_OK, gi);
+        finish();
+    }
+}
